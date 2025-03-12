cffaaf795dce0ec05722f1556767a2d1793897e0
el-collins <charlescollins476@gmail.com>
2025-03-12 09:54:42 +0100

    ✨ add Game and Genre entities, configure SQLite connection, and update appsettings.json : 2025-03-12 08:54:41 ✨

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using GameStore.Api.Entities;
+namespace GameStore.Api.Data;
+
+
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+
+}
+
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,15 +1,20 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 // Register the endpoints   
 app.MapGamesEndpoints();
-
-
 
 
 app.Run();
